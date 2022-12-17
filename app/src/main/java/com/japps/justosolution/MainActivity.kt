00005f2185494855c3a137c38503c3a9553dd62b
Josue Marfil <josue7@users.noreply.github.com>
2022-12-17 05:21:33 -0600

    Validación de checbox
    Se valido que se haya seleccionado alguna opción para agregarla a la cadena

--- app/src/main/java/com/japps/justosolution/MainActivity.kt
+++ app/src/main/java/com/japps/justosolution/MainActivity.kt
@@ -2,11 +2,49 @@ package com.japps.justosolution
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.japps.justosolution.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binder: ActivityMainBinding
+    private lateinit var attributesAPI: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val numberResult = resources.getStringArray(R.array.number_result)
+
+        binder = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binder.root)
+
+        val arrAdapter = ArrayAdapter(this, R.layout.dropdown_item, numberResult)
+        binder.actvResult.setAdapter(arrAdapter)
+
+        binder.btnDataGenerate.setOnClickListener{
+            attributesAPI = generateQueryGender()
+        }
+    }
+
+    private fun generateQueryGender(): String {
+        var query = ""
+        if (binder.cbMale.isChecked == true && binder.cbFemale.isChecked == true){
+            binder.cbMale.error = "Solo elegir un genero"
+            binder.cbMale.requestFocus()
+        } else {
+            if (binder.cbMale.isChecked == true) {
+                query += "male"
+            }
+            if (binder.cbFemale.isChecked == true){
+                query += "female"
+            }
+        }
+        return query
+    }
+
+    private fun generateQueryNationaliti(): String {
+        var query = ArrayList<String>()
+        if (binder.cbBr.isChecked == true){
+            query.add(resources.getString(R.string.br))
+        }
     }
 }
